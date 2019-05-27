32b45ca30b1bab92daa85446a5b09b8871f259d6
ZerefPk <50809136+ZerefPk@users.noreply.github.com>
2019-05-27 11:00:14 -0400

    ---

--- public/controller/LoginAlunoController.php
+++ public/controller/LoginAlunoController.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../model/Aluno.php';
+include_once '../../model/Aluno.php';
 session_start();
 $senha = $_POST['senha'];
 $cpf   = $_POST['cpf'];
@@ -9,16 +9,16 @@ if (isset($senha) && isset($cpf)) {
     $aluno = $aluno->loginAluno($cpf, $senha);
 
     if ($aluno) {
-        header('location: ../public/indexAlunoCadastrados.php');
+        header('location: ../indexAlunoCadastrados.php');
     }
     else {
         echo "<script>alert('CPF OU SENHA INVALIDO');</script>";
-    	echo "<script>location.href='../public/index.php';</script>";
+    	echo "<script>location.href='../index.php';</script>";
     }
 }
 else {
     echo "<script>alert('CPF OU SENHA INVALIDO');</script>";
-	echo "<script>location.href='../public/index.php';</script>";
+	echo "<script>location.href='../index.php';</script>";
 }
 
 
